98b80209e3204afcfc78fceca3c66de5224f9c72
radoone <radoone@gmail.com>
2021-04-18 21:36:50 +0200

    Update prsi.py

--- app/environments/prsi/prsi/envs/prsi.py
+++ app/environments/prsi/prsi/envs/prsi.py
@@ -1,4 +1,3 @@
-
 import gym
 import numpy as np
 import config
@@ -15,8 +14,6 @@ class PrsiEnv(gym.Env):
         self.manual = manual
         self.played_cards = []
         self.verbose = verbose
-        self.zobrane3 = False
-        self.stalsom = False
 
         self.n_players = 2
         self.cards_per_player = 5
@@ -31,18 +28,14 @@ class PrsiEnv(gym.Env):
         self.observation_space = gym.spaces.Box(
             low=0, high=1, shape=(280,))
 
-    #    self.action_space = gym.spaces.Discrete(
-     #       15 + 15 * 15)
-  #      self.observation_space = gym.spaces.Box(
-   #         0, 1, (self.total_cards + self.n_players + self.action_space.n,))
-
         self.verbose = verbose
         self.zobrane3 = False
         self.stalsom = False
+        self.menimna = False
+        self.ktoryvysnik = False
 
     @ property
     def observation(self):
-
         obs = np.zeros(([6, 35]))
         maybe = np.zeros(35)
 
@@ -61,13 +54,6 @@ class PrsiEnv(gym.Env):
         for card in self.played_cards:
             obs[3][card.id] = 1
             maybe[card.id] = 1
-
-        # # pocet protivnikovych kariet
-        # # TODO: opravit pre viac hracou
-        # oponet_card_sum = 0
-
-        # for cardid in range(1, 32):
-       #     if self.played_cards.count(cardid)
 
         for i in range(self.n_players):
             obs[4+i][len(self.players[i].hand.cards)] = 1
@@ -95,65 +81,35 @@ class PrsiEnv(gym.Env):
             legal_actions[0] = 0
             legal_actions[34] = 1
         else:
-
             for card in hand:
-                if card.suit == self.tableCard.suit or card.name == self.tableCard.name or card.name == "Vysnik":
+                if card.name == "Vysnik":
+                    # TODO: opravit viac vysnikov na ruke
+                    self.ktoryvysnik = card.id
+                    # meni na  Gula
+                    legal_actions[6] = 1
+                    # meni na  Zalud
+                    legal_actions[14] = 1
+                    # # meni na  Zelen
+                    legal_actions[22] = 1
+                    # # meni na cerven
+                    legal_actions[30] = 1
+
+                elif (card.suit == self.menimna):
                     legal_actions[card.id] = 1
+
+                elif (not self.menimna and (card.suit == self.tableCard.suit or card.name == self.tableCard.name)):
+                    legal_actions[card.id] = 1
+
         return legal_actions
-
-    def score_game(self):
-        reward = [0.0] * self.n_players
-        scores = [p.score for p in self.players]
-        best_score = max(scores)
-        worst_score = min(scores)
-        winners = []
-        losers = []
-        for i, s in enumerate(scores):
-            if s == best_score:
-                winners.append(i)
-            if s == worst_score:
-                losers.append(i)
-
-        for w in winners:
-            reward[w] += 1.0 / len(winners)
-
-        for l in losers:
-            reward[l] -= 1.0 / len(losers)
-
-        return reward
-
-    def get_limits(self, counts, type):
-        counts = np.array(counts, dtype=np.float)
-        if type == 'max':
-            type_counts = np.nanmax(counts)
-        else:
-            type_counts = np.nanmin(counts)
-
-        counts_winners = []
-
-        for i, m in enumerate(counts):
-            if m == type_counts:
-                counts_winners.append(i)
-
-        return counts_winners
 
     @ property
     def current_player(self):
         return self.players[self.current_player_num]
 
-    def convert_action(self, action):
-        if action < self.card_types:
-            return False, action, None
-        else:
-            action = action - self.card_types
-            first_card = action // self.card_types
-            second_card = action % self.card_types
-            return True, first_card, second_card
-
     def step(self, action):
-
         reward = [0] * self.n_players
         done = False
+        menimna = self.menimna
 
         # check if have card in deck
         if self.deck.size() == 0 or (action == 34 and self.deck.size() < 3):
@@ -177,6 +133,22 @@ class PrsiEnv(gym.Env):
             elif action == 33:
                 self.stalsom = True
             else:
+                self.menimna = ""
+                menimna = ""
+
+                if action == 6:
+                    action = self.ktoryvysnik
+                    menimna = "Gula"
+                elif action == 14:
+                    action = self.ktoryvysnik
+                    menimna = "Zalud"
+                elif action == 22:
+                    action = self.ktoryvysnik
+                    menimna = "Zelen"
+                elif action == 30:
+                    action = self.ktoryvysnik
+                    menimna = "Cerven"
+
                 self.zobrane3 = False
                 self.stalsom = False
 
@@ -189,14 +161,11 @@ class PrsiEnv(gym.Env):
             done = True
         self.done = done
         self.round += 1
+
         self.current_player_num = (
             self.current_player_num + 1) % self.n_players
-
+        self.menimna = menimna
         return self.observation, reward, done, {}
-
-    def reset_round(self):
-        self.round += 1
-        self.turns_taken = 0
 
     def reset(self):
         self.round = 0
@@ -207,6 +176,10 @@ class PrsiEnv(gym.Env):
 
         self.zobrane3 = False
         self.stalsom = False
+        self.zobrane3 = False
+        self.stalsom = False
+        self.menimna = ""
+        self.ktoryvysnik = ""
 
         player_id = 1
         for p in range(self.n_players):
@@ -219,7 +192,6 @@ class PrsiEnv(gym.Env):
         self.current_player_num = 0
         self.done = False
         self.tableCard = self.deck.pop()[0]
-        self.reset_round()
         logger.debug(f'\n\n---- NEW GAME ----')
         return self.observation
 
@@ -239,9 +211,11 @@ class PrsiEnv(gym.Env):
                 logger.debug(card.id, card.name, card.suit)
 
         logger.debug(
-            f'\nTable card {self.tableCard.name} {self.tableCard.suit}')
+            f'\nTable card: {self.tableCard.name} {self.tableCard.suit}')
 
         logger.debug(f'\n{self.deck.size()} cards left in deck')
+        if self.menimna:
+            logger.debug(f'\nMenim na {self.menimna}')
 
         if self.verbose:
             logger.debug(
@@ -254,10 +228,6 @@ class PrsiEnv(gym.Env):
         if self.done:
             logger.debug(f'\n\nRound {self.round}')
             logger.debug(f'\n\nGAME OVER')
-
-        if self.turns_taken == self.cards_per_player:
-            for p in self.players:
-                logger.debug(f'Player {p.id} points: {p.score}')
 
     def rules_move(self):
         raise Exception(
