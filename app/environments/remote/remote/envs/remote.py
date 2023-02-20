68bf1ba7d29d0b2d5d0762dd7e4a3435face07d5
Dan Bravender <dan.bravender@gmail.com>
2023-02-20 08:52:47 -0500

    Reuse TCP connections for remote training

--- app/environments/remote/remote/envs/remote.py
+++ app/environments/remote/remote/envs/remote.py
@@ -17,6 +17,7 @@ class RemoteEnv(gym.Env):
         super(RemoteEnv, self).__init__()
         self.name = 'remote'
         self.manual = manual
+        self.session = requests.Session()
         self.newgame()
 
         self.action_space = gym.spaces.Discrete(self.action_space_size)
@@ -65,12 +66,12 @@ class RemoteEnv(gym.Env):
     def remote_call(self, endpoint, data=None):
         url = f'{BASE_URL}/{endpoint}/{self.remote_id}'
         if data is None:
-            return requests.get(url).json()
+            return self.session.get(url).json()
         else:
-            return requests.post(url, json=data).json()
+            return self.session.post(url, json=data).json()
 
     def newgame(self):
-        response = requests.get(f'{BASE_URL}/newgame').json()
+        response = self.session.get(f'{BASE_URL}/newgame').json()
         self.remote_id = response['id']
         self.action_space_size = response['action_space_size']
         self.observation_space_size = response['observation_space_size']
